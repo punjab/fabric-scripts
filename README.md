4b6606e917cd89fe852e9c1691a52120650a6434
Punjab <punjcoder@gmail.com>
2012-04-16 17:49:43 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+## Fabric Scripts
+
+Fabric scripts for various simple tasks go here.
+
+To install Fabric
+  
+  pip install fabric
+
+The script assumes [Public Key Based Authentication](https://hkn.eecs.berkeley.edu/~dhsu/ssh_public_key_howto.html) and use of [SSH Config file](http://nerderati.com/2011/03/simplify-your-life-with-an-ssh-config-file/).
+
+### Bibliography
++ http://nopper.archpwn.org/2010/08/09/backup-your-django-database-using-fabric/
---
+++ fabfile.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+from fabric.api import *
+from fabric.operations import get
+env.use_ssh_config = True # read ~/.ssh/.sshconfig
+
+import time
+from os.path import basename, exists
+
+env.hosts = ['mywebapp.com']
+env.dbname = 'mydatabase'
+env.dbuser = 'myusername'
+env.dbpass = 'mypassword'
+
+def backup_mysql():    
+    date = time.strftime('%Y%m%d%H%M%S')
+    filename = '/tmp/%(database)s-backup-%(date)s.sql.gz' % { 
+        'database': env.dbname,
+        'date': date
+    }
+
+    if exists(filename):
+        run('rm "%s"' % filename)
+        
+    run('mysqldump -u %(username)s -p%(password)s %(database)s | gzip > %(filename)s' % {
+            'username': env.dbuser,
+            'password': env.dbpass,
+            'database': env.dbname,
+            'filename': filename
+    })
+    
+    get(filename, os.path.basename(filename))
+    run('rm "%s"' % filename)
